3eb5eebabf454cc44b5617bc773efd149c39ee1b
Yorx1 <jorge18xdd@gmail.com>
2022-11-28 04:16:30 -0500

    Se agregaron formularios vacios

--- InmuebleSetorRegistro.cs
+++ InmuebleSetorRegistro.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ProyectoCoronado
 {
-    public partial class Form1 : Form
+    public partial class InmuebleSetorRegistro : Form
     {
-        public Form1()
+        public InmuebleSetorRegistro()
         {
             InitializeComponent();
         }

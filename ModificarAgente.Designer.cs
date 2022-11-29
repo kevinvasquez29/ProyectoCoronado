6806ffa6e8a1e63c6eee14eb64b67aaaf9a473a2
Yorx1 <jorge18xdd@gmail.com>
2022-11-28 21:30:18 -0500

    ModificarAgente - ModificarCita (diseño)

--- ModificarAgente.Designer.cs
+++ ModificarAgente.Designer.cs
@@ -28,12 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lbId = new System.Windows.Forms.Label();
+            this.btDeshabilitar = new System.Windows.Forms.Button();
+            this.chbHabilitado = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtContraseñaDatosCliente = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDatosCorreoElectronica = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelDatosCliente = new System.Windows.Forms.Label();
+            this.txtCelularAgente = new System.Windows.Forms.TextBox();
+            this.txtApellidoAgente = new System.Windows.Forms.TextBox();
+            this.txtNombreAgente = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(161, 168);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(132, 22);
+            this.txtId.TabIndex = 58;
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbId.Location = new System.Drawing.Point(23, 166);
+            this.lbId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(42, 25);
+            this.lbId.TabIndex = 57;
+            this.lbId.Text = "Id:";
+            // 
+            // btDeshabilitar
+            // 
+            this.btDeshabilitar.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDeshabilitar.Location = new System.Drawing.Point(704, 304);
+            this.btDeshabilitar.Margin = new System.Windows.Forms.Padding(4);
+            this.btDeshabilitar.Name = "btDeshabilitar";
+            this.btDeshabilitar.Size = new System.Drawing.Size(251, 66);
+            this.btDeshabilitar.TabIndex = 56;
+            this.btDeshabilitar.Text = "Deshabilitar";
+            this.btDeshabilitar.UseVisualStyleBackColor = true;
+            // 
+            // chbHabilitado
+            // 
+            this.chbHabilitado.AutoSize = true;
+            this.chbHabilitado.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbHabilitado.Location = new System.Drawing.Point(253, 491);
+            this.chbHabilitado.Margin = new System.Windows.Forms.Padding(4);
+            this.chbHabilitado.Name = "chbHabilitado";
+            this.chbHabilitado.Size = new System.Drawing.Size(157, 26);
+            this.chbHabilitado.TabIndex = 55;
+            this.chbHabilitado.Text = "Habilitado";
+            this.chbHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 416);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 25);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "Contraseña:";
+            // 
+            // txtContraseñaDatosCliente
+            // 
+            this.txtContraseñaDatosCliente.Location = new System.Drawing.Point(204, 416);
+            this.txtContraseñaDatosCliente.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContraseñaDatosCliente.Name = "txtContraseñaDatosCliente";
+            this.txtContraseñaDatosCliente.Size = new System.Drawing.Size(436, 22);
+            this.txtContraseñaDatosCliente.TabIndex = 53;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 363);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(272, 25);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "Correo electronico:";
+            // 
+            // txtDatosCorreoElectronica
+            // 
+            this.txtDatosCorreoElectronica.Location = new System.Drawing.Point(301, 363);
+            this.txtDatosCorreoElectronica.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDatosCorreoElectronica.Name = "txtDatosCorreoElectronica";
+            this.txtDatosCorreoElectronica.Size = new System.Drawing.Size(339, 22);
+            this.txtDatosCorreoElectronica.TabIndex = 51;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(734, 467);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(251, 66);
+            this.btnCancelar.TabIndex = 50;
+            this.btnCancelar.Text = "Regresar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(704, 213);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(251, 66);
+            this.btnModificar.TabIndex = 49;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 304);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 25);
+            this.label4.TabIndex = 48;
+            this.label4.Text = "Celular:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 252);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Apellido:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 205);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Nombre:";
+            // 
+            // labelDatosCliente
+            // 
+            this.labelDatosCliente.AutoSize = true;
+            this.labelDatosCliente.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.labelDatosCliente.Font = new System.Drawing.Font("Algerian", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDatosCliente.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelDatosCliente.Location = new System.Drawing.Point(41, 44);
+            this.labelDatosCliente.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDatosCliente.Name = "labelDatosCliente";
+            this.labelDatosCliente.Size = new System.Drawing.Size(594, 66);
+            this.labelDatosCliente.TabIndex = 45;
+            this.labelDatosCliente.Text = "Modificar Agente";
+            // 
+            // txtCelularAgente
+            // 
+            this.txtCelularAgente.Location = new System.Drawing.Point(169, 304);
+            this.txtCelularAgente.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCelularAgente.Name = "txtCelularAgente";
+            this.txtCelularAgente.Size = new System.Drawing.Size(181, 22);
+            this.txtCelularAgente.TabIndex = 43;
+            // 
+            // txtApellidoAgente
+            // 
+            this.txtApellidoAgente.Location = new System.Drawing.Point(161, 252);
+            this.txtApellidoAgente.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidoAgente.Name = "txtApellidoAgente";
+            this.txtApellidoAgente.Size = new System.Drawing.Size(427, 22);
+            this.txtApellidoAgente.TabIndex = 42;
+            // 
+            // txtNombreAgente
+            // 
+            this.txtNombreAgente.Location = new System.Drawing.Point(161, 205);
+            this.txtNombreAgente.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombreAgente.Name = "txtNombreAgente";
+            this.txtNombreAgente.Size = new System.Drawing.Size(427, 22);
+            this.txtNombreAgente.TabIndex = 41;
+            // 
+            // ModificarAgente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(983, 564);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.btDeshabilitar);
+            this.Controls.Add(this.chbHabilitado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtContraseñaDatosCliente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDatosCorreoElectronica);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDatosCliente);
+            this.Controls.Add(this.txtCelularAgente);
+            this.Controls.Add(this.txtApellidoAgente);
+            this.Controls.Add(this.txtNombreAgente);
+            this.Name = "ModificarAgente";
             this.Text = "ModificarAgente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.Button btDeshabilitar;
+        private System.Windows.Forms.CheckBox chbHabilitado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtContraseñaDatosCliente;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDatosCorreoElectronica;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDatosCliente;
+        private System.Windows.Forms.TextBox txtCelularAgente;
+        private System.Windows.Forms.TextBox txtApellidoAgente;
+        private System.Windows.Forms.TextBox txtNombreAgente;
     }
 }
--- ModificarBanco.Designer.cs
+++ ModificarBanco.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkBoxHabilitar = new System.Windows.Forms.CheckBox();
+            this.btnDeshabilitar = new System.Windows.Forms.Button();
+            this.txtIdBanco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label1Bnaco = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1NombreBanco = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBoxHabilitar
+            // 
+            this.checkBoxHabilitar.AutoSize = true;
+            this.checkBoxHabilitar.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxHabilitar.Location = new System.Drawing.Point(90, 290);
+            this.checkBoxHabilitar.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBoxHabilitar.Name = "checkBoxHabilitar";
+            this.checkBoxHabilitar.Size = new System.Drawing.Size(159, 31);
+            this.checkBoxHabilitar.TabIndex = 55;
+            this.checkBoxHabilitar.Text = "Habilitado";
+            this.checkBoxHabilitar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeshabilitar
+            // 
+            this.btnDeshabilitar.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeshabilitar.Location = new System.Drawing.Point(94, 436);
+            this.btnDeshabilitar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeshabilitar.Name = "btnDeshabilitar";
+            this.btnDeshabilitar.Size = new System.Drawing.Size(224, 37);
+            this.btnDeshabilitar.TabIndex = 54;
+            this.btnDeshabilitar.Text = "Deshabilitar";
+            this.btnDeshabilitar.UseVisualStyleBackColor = true;
+            // 
+            // txtIdBanco
+            // 
+            this.txtIdBanco.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdBanco.Location = new System.Drawing.Point(90, 197);
+            this.txtIdBanco.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIdBanco.Multiline = true;
+            this.txtIdBanco.Name = "txtIdBanco";
+            this.txtIdBanco.Size = new System.Drawing.Size(113, 40);
+            this.txtIdBanco.TabIndex = 53;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 27);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Id Banco";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(620, 494);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(224, 37);
+            this.btnSalir.TabIndex = 51;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(94, 379);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(224, 37);
+            this.btnGuardar.TabIndex = 50;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label1Bnaco
+            // 
+            this.label1Bnaco.AutoSize = true;
+            this.label1Bnaco.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1Bnaco.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Bnaco.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1Bnaco.Location = new System.Drawing.Point(169, -1);
+            this.label1Bnaco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1Bnaco.Name = "label1Bnaco";
+            this.label1Bnaco.Size = new System.Drawing.Size(521, 97);
+            this.label1Bnaco.TabIndex = 49;
+            this.label1Bnaco.Text = "Modificar Banco";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(443, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 27);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Ingrese Nuevo Nombre de Banco";
+            // 
+            // textBox1NombreBanco
+            // 
+            this.textBox1NombreBanco.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1NombreBanco.Location = new System.Drawing.Point(435, 197);
+            this.textBox1NombreBanco.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1NombreBanco.Multiline = true;
+            this.textBox1NombreBanco.Name = "textBox1NombreBanco";
+            this.textBox1NombreBanco.Size = new System.Drawing.Size(409, 40);
+            this.textBox1NombreBanco.TabIndex = 47;
+            // 
+            // ModificarBanco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(891, 545);
+            this.Controls.Add(this.checkBoxHabilitar);
+            this.Controls.Add(this.btnDeshabilitar);
+            this.Controls.Add(this.txtIdBanco);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label1Bnaco);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1NombreBanco);
+            this.Name = "ModificarBanco";
             this.Text = "ModificarBanco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxHabilitar;
+        private System.Windows.Forms.Button btnDeshabilitar;
+        private System.Windows.Forms.TextBox txtIdBanco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label1Bnaco;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1NombreBanco;
     }
 }

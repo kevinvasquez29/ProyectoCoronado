6abe10586149447354d06b5b20b03a5faea00e10
Yorx1 <jorge18xdd@gmail.com>
2022-11-28 20:40:08 -0500

    Se agrego Menu Principal

--- MModificarTerreno.Designer.cs
+++ MModificarTerreno.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MModificarTerreno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1002, 582);
+            this.Name = "MModificarTerreno";
             this.Text = "MModificarTerreno";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MenuPrincipal.Designer.cs
+++ MenuPrincipal.Designer.cs
@@ -28,24 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btSector = new System.Windows.Forms.Button();
+            this.btCita = new System.Windows.Forms.Button();
+            this.btVenta = new System.Windows.Forms.Button();
+            this.btInmueble = new System.Windows.Forms.Button();
+            this.btPropietario = new System.Windows.Forms.Button();
+            this.btSitioInteres = new System.Windows.Forms.Button();
+            this.btCiudad = new System.Windows.Forms.Button();
+            this.btBanco = new System.Windows.Forms.Button();
+            this.btAgente = new System.Windows.Forms.Button();
+            this.btCliente = new System.Windows.Forms.Button();
+            this.label1Bnaco = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // btSalir
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(442, 132);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.btSalir.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalir.Location = new System.Drawing.Point(760, 541);
+            this.btSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(127, 57);
+            this.btSalir.TabIndex = 67;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            // 
+            // btSector
+            // 
+            this.btSector.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSector.Location = new System.Drawing.Point(522, 465);
+            this.btSector.Margin = new System.Windows.Forms.Padding(4);
+            this.btSector.Name = "btSector";
+            this.btSector.Size = new System.Drawing.Size(224, 37);
+            this.btSector.TabIndex = 66;
+            this.btSector.Text = "Sector";
+            this.btSector.UseVisualStyleBackColor = true;
+            // 
+            // btCita
+            // 
+            this.btCita.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCita.Location = new System.Drawing.Point(522, 379);
+            this.btCita.Margin = new System.Windows.Forms.Padding(4);
+            this.btCita.Name = "btCita";
+            this.btCita.Size = new System.Drawing.Size(224, 37);
+            this.btCita.TabIndex = 65;
+            this.btCita.Text = "Cita";
+            this.btCita.UseVisualStyleBackColor = true;
+            // 
+            // btVenta
+            // 
+            this.btVenta.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btVenta.Location = new System.Drawing.Point(522, 291);
+            this.btVenta.Margin = new System.Windows.Forms.Padding(4);
+            this.btVenta.Name = "btVenta";
+            this.btVenta.Size = new System.Drawing.Size(224, 37);
+            this.btVenta.TabIndex = 64;
+            this.btVenta.Text = "Venta";
+            this.btVenta.UseVisualStyleBackColor = true;
+            // 
+            // btInmueble
+            // 
+            this.btInmueble.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInmueble.Location = new System.Drawing.Point(522, 205);
+            this.btInmueble.Margin = new System.Windows.Forms.Padding(4);
+            this.btInmueble.Name = "btInmueble";
+            this.btInmueble.Size = new System.Drawing.Size(224, 37);
+            this.btInmueble.TabIndex = 63;
+            this.btInmueble.Text = "Inmuebles";
+            this.btInmueble.UseVisualStyleBackColor = true;
+            // 
+            // btPropietario
+            // 
+            this.btPropietario.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPropietario.Location = new System.Drawing.Point(522, 120);
+            this.btPropietario.Margin = new System.Windows.Forms.Padding(4);
+            this.btPropietario.Name = "btPropietario";
+            this.btPropietario.Size = new System.Drawing.Size(224, 37);
+            this.btPropietario.TabIndex = 62;
+            this.btPropietario.Text = "Propietario";
+            this.btPropietario.UseVisualStyleBackColor = true;
+            // 
+            // btSitioInteres
+            // 
+            this.btSitioInteres.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSitioInteres.Location = new System.Drawing.Point(34, 465);
+            this.btSitioInteres.Margin = new System.Windows.Forms.Padding(4);
+            this.btSitioInteres.Name = "btSitioInteres";
+            this.btSitioInteres.Size = new System.Drawing.Size(224, 37);
+            this.btSitioInteres.TabIndex = 61;
+            this.btSitioInteres.Text = "Sitio de Interés";
+            this.btSitioInteres.UseVisualStyleBackColor = true;
+            // 
+            // btCiudad
+            // 
+            this.btCiudad.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btCiudad.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCiudad.Location = new System.Drawing.Point(34, 379);
+            this.btCiudad.Margin = new System.Windows.Forms.Padding(4);
+            this.btCiudad.Name = "btCiudad";
+            this.btCiudad.Size = new System.Drawing.Size(224, 37);
+            this.btCiudad.TabIndex = 60;
+            this.btCiudad.Text = "Ciudad";
+            this.btCiudad.UseVisualStyleBackColor = true;
+            // 
+            // btBanco
+            // 
+            this.btBanco.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBanco.Location = new System.Drawing.Point(34, 291);
+            this.btBanco.Margin = new System.Windows.Forms.Padding(4);
+            this.btBanco.Name = "btBanco";
+            this.btBanco.Size = new System.Drawing.Size(224, 37);
+            this.btBanco.TabIndex = 59;
+            this.btBanco.Text = "Banco";
+            this.btBanco.UseVisualStyleBackColor = true;
+            // 
+            // btAgente
+            // 
+            this.btAgente.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAgente.Location = new System.Drawing.Point(34, 205);
+            this.btAgente.Margin = new System.Windows.Forms.Padding(4);
+            this.btAgente.Name = "btAgente";
+            this.btAgente.Size = new System.Drawing.Size(224, 37);
+            this.btAgente.TabIndex = 58;
+            this.btAgente.Text = "Agente";
+            this.btAgente.UseVisualStyleBackColor = true;
+            // 
+            // btCliente
+            // 
+            this.btCliente.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCliente.Location = new System.Drawing.Point(34, 120);
+            this.btCliente.Margin = new System.Windows.Forms.Padding(4);
+            this.btCliente.Name = "btCliente";
+            this.btCliente.Size = new System.Drawing.Size(224, 37);
+            this.btCliente.TabIndex = 57;
+            this.btCliente.Text = "Cliente";
+            this.btCliente.UseVisualStyleBackColor = true;
+            // 
+            // label1Bnaco
+            // 
+            this.label1Bnaco.AutoSize = true;
+            this.label1Bnaco.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1Bnaco.Font = new System.Drawing.Font("Algerian", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Bnaco.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1Bnaco.Location = new System.Drawing.Point(277, 25);
+            this.label1Bnaco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1Bnaco.Name = "label1Bnaco";
+            this.label1Bnaco.Size = new System.Drawing.Size(400, 53);
+            this.label1Bnaco.TabIndex = 56;
+            this.label1Bnaco.Text = "Menu Principal";
             // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(905, 611);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.btSector);
+            this.Controls.Add(this.btCita);
+            this.Controls.Add(this.btVenta);
+            this.Controls.Add(this.btInmueble);
+            this.Controls.Add(this.btPropietario);
+            this.Controls.Add(this.btSitioInteres);
+            this.Controls.Add(this.btCiudad);
+            this.Controls.Add(this.btBanco);
+            this.Controls.Add(this.btAgente);
+            this.Controls.Add(this.btCliente);
+            this.Controls.Add(this.label1Bnaco);
             this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
             this.ResumeLayout(false);
@@ -55,6 +203,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btSector;
+        private System.Windows.Forms.Button btCita;
+        private System.Windows.Forms.Button btVenta;
+        private System.Windows.Forms.Button btInmueble;
+        private System.Windows.Forms.Button btPropietario;
+        private System.Windows.Forms.Button btSitioInteres;
+        private System.Windows.Forms.Button btCiudad;
+        private System.Windows.Forms.Button btBanco;
+        private System.Windows.Forms.Button btAgente;
+        private System.Windows.Forms.Button btCliente;
+        private System.Windows.Forms.Label label1Bnaco;
     }
 }

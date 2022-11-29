53663cc8717fa73dd802da51aa1db3b1545c417e
Yorx1 <jorge18xdd@gmail.com>
2022-11-28 21:46:17 -0500

    .2

--- MRegistrarCita.Designer.cs
+++ MRegistrarCita.Designer.cs
@@ -28,12 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxHoraCita = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerCita = new System.Windows.Forms.DateTimePicker();
+            this.txtIdDelAgenteRegistrarCita = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdDelInmuebleRegistrarCita = new System.Windows.Forms.TextBox();
+            this.btnCancelarRegistrarCita = new System.Windows.Forms.Button();
+            this.btnAceptarRegistrarCita = new System.Windows.Forms.Button();
+            this.txtIdDelClienteRegistrarCita = new System.Windows.Forms.TextBox();
+            this.lbIdDelCliente = new System.Windows.Forms.Label();
+            this.lnIdInmuebleRegistrarCita = new System.Windows.Forms.Label();
+            this.lbFechaCita = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(256, 31);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Hora de la Cita";
+            // 
+            // comboBoxHoraCita
+            // 
+            this.comboBoxHoraCita.FormattingEnabled = true;
+            this.comboBoxHoraCita.Items.AddRange(new object[] {
+            "8:00",
+            "9:00",
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00"});
+            this.comboBoxHoraCita.Location = new System.Drawing.Point(324, 165);
+            this.comboBoxHoraCita.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxHoraCita.Name = "comboBoxHoraCita";
+            this.comboBoxHoraCita.Size = new System.Drawing.Size(195, 24);
+            this.comboBoxHoraCita.TabIndex = 41;
+            // 
+            // dateTimePickerCita
+            // 
+            this.dateTimePickerCita.Location = new System.Drawing.Point(337, 106);
+            this.dateTimePickerCita.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePickerCita.Name = "dateTimePickerCita";
+            this.dateTimePickerCita.Size = new System.Drawing.Size(304, 22);
+            this.dateTimePickerCita.TabIndex = 40;
+            // 
+            // txtIdDelAgenteRegistrarCita
+            // 
+            this.txtIdDelAgenteRegistrarCita.Location = new System.Drawing.Point(324, 378);
+            this.txtIdDelAgenteRegistrarCita.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIdDelAgenteRegistrarCita.Name = "txtIdDelAgenteRegistrarCita";
+            this.txtIdDelAgenteRegistrarCita.Size = new System.Drawing.Size(595, 22);
+            this.txtIdDelAgenteRegistrarCita.TabIndex = 39;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 378);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 31);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "ID del Agente";
+            // 
+            // txtIdDelInmuebleRegistrarCita
+            // 
+            this.txtIdDelInmuebleRegistrarCita.Location = new System.Drawing.Point(324, 224);
+            this.txtIdDelInmuebleRegistrarCita.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIdDelInmuebleRegistrarCita.Name = "txtIdDelInmuebleRegistrarCita";
+            this.txtIdDelInmuebleRegistrarCita.Size = new System.Drawing.Size(595, 22);
+            this.txtIdDelInmuebleRegistrarCita.TabIndex = 37;
+            // 
+            // btnCancelarRegistrarCita
+            // 
+            this.btnCancelarRegistrarCita.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarRegistrarCita.Location = new System.Drawing.Point(635, 446);
+            this.btnCancelarRegistrarCita.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelarRegistrarCita.Name = "btnCancelarRegistrarCita";
+            this.btnCancelarRegistrarCita.Size = new System.Drawing.Size(212, 79);
+            this.btnCancelarRegistrarCita.TabIndex = 36;
+            this.btnCancelarRegistrarCita.Text = "Cancelar";
+            this.btnCancelarRegistrarCita.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptarRegistrarCita
+            // 
+            this.btnAceptarRegistrarCita.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptarRegistrarCita.Location = new System.Drawing.Point(276, 446);
+            this.btnAceptarRegistrarCita.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAceptarRegistrarCita.Name = "btnAceptarRegistrarCita";
+            this.btnAceptarRegistrarCita.Size = new System.Drawing.Size(212, 79);
+            this.btnAceptarRegistrarCita.TabIndex = 35;
+            this.btnAceptarRegistrarCita.Text = "Aceptar";
+            this.btnAceptarRegistrarCita.UseVisualStyleBackColor = true;
+            // 
+            // txtIdDelClienteRegistrarCita
+            // 
+            this.txtIdDelClienteRegistrarCita.Location = new System.Drawing.Point(324, 303);
+            this.txtIdDelClienteRegistrarCita.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIdDelClienteRegistrarCita.Name = "txtIdDelClienteRegistrarCita";
+            this.txtIdDelClienteRegistrarCita.Size = new System.Drawing.Size(595, 22);
+            this.txtIdDelClienteRegistrarCita.TabIndex = 34;
+            // 
+            // lbIdDelCliente
+            // 
+            this.lbIdDelCliente.AutoSize = true;
+            this.lbIdDelCliente.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIdDelCliente.Location = new System.Drawing.Point(59, 303);
+            this.lbIdDelCliente.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbIdDelCliente.Name = "lbIdDelCliente";
+            this.lbIdDelCliente.Size = new System.Drawing.Size(224, 31);
+            this.lbIdDelCliente.TabIndex = 33;
+            this.lbIdDelCliente.Text = "ID del Cliente";
+            // 
+            // lnIdInmuebleRegistrarCita
+            // 
+            this.lnIdInmuebleRegistrarCita.AutoSize = true;
+            this.lnIdInmuebleRegistrarCita.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnIdInmuebleRegistrarCita.Location = new System.Drawing.Point(59, 224);
+            this.lnIdInmuebleRegistrarCita.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lnIdInmuebleRegistrarCita.Name = "lnIdInmuebleRegistrarCita";
+            this.lnIdInmuebleRegistrarCita.Size = new System.Drawing.Size(248, 31);
+            this.lnIdInmuebleRegistrarCita.TabIndex = 32;
+            this.lnIdInmuebleRegistrarCita.Text = "ID del Inmueble";
+            // 
+            // lbFechaCita
+            // 
+            this.lbFechaCita.AutoSize = true;
+            this.lbFechaCita.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFechaCita.Location = new System.Drawing.Point(59, 100);
+            this.lbFechaCita.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbFechaCita.Name = "lbFechaCita";
+            this.lbFechaCita.Size = new System.Drawing.Size(270, 31);
+            this.lbFechaCita.TabIndex = 31;
+            this.lbFechaCita.Text = "Fecha de la Cita";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 53);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Registrar Cita";
+            // 
+            // MRegistrarCita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1045, 551);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxHoraCita);
+            this.Controls.Add(this.dateTimePickerCita);
+            this.Controls.Add(this.txtIdDelAgenteRegistrarCita);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdDelInmuebleRegistrarCita);
+            this.Controls.Add(this.btnCancelarRegistrarCita);
+            this.Controls.Add(this.btnAceptarRegistrarCita);
+            this.Controls.Add(this.txtIdDelClienteRegistrarCita);
+            this.Controls.Add(this.lbIdDelCliente);
+            this.Controls.Add(this.lnIdInmuebleRegistrarCita);
+            this.Controls.Add(this.lbFechaCita);
+            this.Controls.Add(this.label1);
+            this.Name = "MRegistrarCita";
             this.Text = "MRegistrarCita";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxHoraCita;
+        private System.Windows.Forms.DateTimePicker dateTimePickerCita;
+        private System.Windows.Forms.TextBox txtIdDelAgenteRegistrarCita;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdDelInmuebleRegistrarCita;
+        private System.Windows.Forms.Button btnCancelarRegistrarCita;
+        private System.Windows.Forms.Button btnAceptarRegistrarCita;
+        private System.Windows.Forms.TextBox txtIdDelClienteRegistrarCita;
+        private System.Windows.Forms.Label lbIdDelCliente;
+        private System.Windows.Forms.Label lnIdInmuebleRegistrarCita;
+        private System.Windows.Forms.Label lbFechaCita;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MRegistrarSitioInteres.Designer.cs
+++ MRegistrarSitioInteres.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelRegistrarReseñaSitioInteres = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigoRegistrarSI = new System.Windows.Forms.TextBox();
+            this.txtRegistrarNombreSI = new System.Windows.Forms.TextBox();
+            this.txtRegistrarDireccionSI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelRegistrarReseñaSitioInteres
+            // 
+            this.labelRegistrarReseñaSitioInteres.AutoSize = true;
+            this.labelRegistrarReseñaSitioInteres.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.labelRegistrarReseñaSitioInteres.Font = new System.Drawing.Font("Algerian", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistrarReseñaSitioInteres.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelRegistrarReseñaSitioInteres.Location = new System.Drawing.Point(58, 65);
+            this.labelRegistrarReseñaSitioInteres.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelRegistrarReseñaSitioInteres.Name = "labelRegistrarReseñaSitioInteres";
+            this.labelRegistrarReseñaSitioInteres.Size = new System.Drawing.Size(919, 49);
+            this.labelRegistrarReseñaSitioInteres.TabIndex = 47;
+            this.labelRegistrarReseñaSitioInteres.Text = "Registrar Reseñas de Sitio de Interés";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(800, 545);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(213, 60);
+            this.btnCancelar.TabIndex = 46;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(178, 522);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(213, 60);
+            this.btnAgregar.TabIndex = 45;
+            this.btnAgregar.Text = "Registrar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 411);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(390, 25);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Dirección del Sitio de Interés";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 281);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(365, 25);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Nombre del Sitio de Interés";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 153);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Codigo";
+            // 
+            // txtCodigoRegistrarSI
+            // 
+            this.txtCodigoRegistrarSI.Location = new System.Drawing.Point(67, 194);
+            this.txtCodigoRegistrarSI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigoRegistrarSI.Name = "txtCodigoRegistrarSI";
+            this.txtCodigoRegistrarSI.ReadOnly = true;
+            this.txtCodigoRegistrarSI.Size = new System.Drawing.Size(173, 22);
+            this.txtCodigoRegistrarSI.TabIndex = 40;
+            // 
+            // txtRegistrarNombreSI
+            // 
+            this.txtRegistrarNombreSI.Location = new System.Drawing.Point(67, 329);
+            this.txtRegistrarNombreSI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegistrarNombreSI.Name = "txtRegistrarNombreSI";
+            this.txtRegistrarNombreSI.Size = new System.Drawing.Size(484, 22);
+            this.txtRegistrarNombreSI.TabIndex = 39;
+            // 
+            // txtRegistrarDireccionSI
+            // 
+            this.txtRegistrarDireccionSI.Location = new System.Drawing.Point(67, 466);
+            this.txtRegistrarDireccionSI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegistrarDireccionSI.Name = "txtRegistrarDireccionSI";
+            this.txtRegistrarDireccionSI.Size = new System.Drawing.Size(484, 22);
+            this.txtRegistrarDireccionSI.TabIndex = 38;
+            // 
+            // MRegistrarSitioInteres
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1040, 618);
+            this.Controls.Add(this.labelRegistrarReseñaSitioInteres);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodigoRegistrarSI);
+            this.Controls.Add(this.txtRegistrarNombreSI);
+            this.Controls.Add(this.txtRegistrarDireccionSI);
+            this.Name = "MRegistrarSitioInteres";
             this.Text = "MRegistrarSitioInteres";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelRegistrarReseñaSitioInteres;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigoRegistrarSI;
+        private System.Windows.Forms.TextBox txtRegistrarNombreSI;
+        private System.Windows.Forms.TextBox txtRegistrarDireccionSI;
     }
 }
